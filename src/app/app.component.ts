ab8d93afe15f1bae8f05a5b4a77b1d1b707b665b
Victor Hugo Rosa <victorhugolgr@gmail.com>
2025-02-03 16:42:45 -0300

    feature: navigation, masterhead

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MainComponent} from './page/main/main.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'sandy-arq-landing-page';
 }
