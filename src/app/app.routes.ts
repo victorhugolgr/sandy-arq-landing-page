2b0d3a0dd9a65d801abf6849e0ad797609d8c710
Victor Hugo Rosa <victorhugolgr@gmail.com>
2025-02-08 00:37:57 -0300

    feature: about, service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import {MainComponent} from './page/main/main.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: MainComponent }
+  { path: '', component: MainComponent},
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
--- src/app/page/main/main.component.ts
+++ src/app/page/main/main.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {NavigationComponent} from './navigation/navigation.component';
 import {MasterheadComponent} from './masterhead/masterhead.component';
+import {AboutComponent} from './about/about.component';
+import {ServiceComponent} from './service/service.component';
 
 @Component({
   selector: 'app-main',
-  imports: [NavigationComponent, MasterheadComponent],
+  imports: [NavigationComponent, MasterheadComponent, AboutComponent, ServiceComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
