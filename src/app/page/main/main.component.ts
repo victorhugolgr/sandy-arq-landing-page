ba7b6481bb511f76d7ab96092c41cabb8b285dfb
Victor Hugo Rosa <victorhugolgr@gmail.com>
2025-02-08 16:40:07 -0300

    feature: project , how-works

--- src/app/page/main/main.component.ts
+++ src/app/page/main/main.component.ts
@@ -4,10 +4,11 @@ import {MasterheadComponent} from './masterhead/masterhead.component';
 import {AboutComponent} from './about/about.component';
 import {ServiceComponent} from './service/service.component';
 import {ProjectComponent} from './project/project.component';
+import {HowWorksComponent} from './how-works/how-works.component';
 
 @Component({
   selector: 'app-main',
-  imports: [NavigationComponent, MasterheadComponent, AboutComponent, ServiceComponent, ProjectComponent],
+  imports: [NavigationComponent, MasterheadComponent, AboutComponent, ServiceComponent, ProjectComponent, HowWorksComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
