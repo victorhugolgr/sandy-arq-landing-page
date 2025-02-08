172e6ec0196bbea8da81226ee1e3c94f179390c2
Victor Hugo Rosa <victorhugolgr@gmail.com>
2025-02-08 01:09:05 -0300

    feature: project [wip]

--- src/app/page/main/main.component.ts
+++ src/app/page/main/main.component.ts
@@ -3,10 +3,11 @@ import {NavigationComponent} from './navigation/navigation.component';
 import {MasterheadComponent} from './masterhead/masterhead.component';
 import {AboutComponent} from './about/about.component';
 import {ServiceComponent} from './service/service.component';
+import {ProjectComponent} from './project/project.component';
 
 @Component({
   selector: 'app-main',
-  imports: [NavigationComponent, MasterheadComponent, AboutComponent, ServiceComponent],
+  imports: [NavigationComponent, MasterheadComponent, AboutComponent, ServiceComponent, ProjectComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
