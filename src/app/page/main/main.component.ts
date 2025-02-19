12f905c0e3a9551c08bdd20a2978fbe861730d21
Victor Hugo Rosa <victorhugolgr@gmail.com>
2025-02-19 15:20:29 -0300

    feature: finish incial project

--- src/app/page/main/main.component.ts
+++ src/app/page/main/main.component.ts
@@ -5,10 +5,11 @@ import {AboutComponent} from './about/about.component';
 import {ServiceComponent} from './service/service.component';
 import {ProjectComponent} from './project/project.component';
 import {HowWorksComponent} from './how-works/how-works.component';
+import {ContactUsComponent} from './contact-us/contact-us.component';
 
 @Component({
   selector: 'app-main',
-  imports: [NavigationComponent, MasterheadComponent, AboutComponent, ServiceComponent, ProjectComponent, HowWorksComponent],
+  imports: [NavigationComponent, MasterheadComponent, AboutComponent, ServiceComponent, ProjectComponent, HowWorksComponent, ContactUsComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
