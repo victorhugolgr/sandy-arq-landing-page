35cec8b29239b301817d9c66a6c2a5dbe82c2ba8
Victor Hugo Rosa <victorhugolgr@gmail.com>
2025-02-23 00:23:12 -0300

    feature: finish incial project

--- src/app/page/main/main.component.ts
+++ src/app/page/main/main.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {NavigationComponent} from './navigation/navigation.component';
 import {MasterheadComponent} from './masterhead/masterhead.component';
 import {AboutComponent} from './about/about.component';
 import {ServiceComponent} from './service/service.component';
 import {ProjectComponent} from './project/project.component';
-import {HowWorksComponent} from './how-works/how-works.component';
 import {ContactUsComponent} from './contact-us/contact-us.component';
+import {WhatappLinkComponent} from './whatapp-link/whatapp-link.component';
+import {FooterComponent} from './footer/footer.component';
 
 @Component({
   selector: 'app-main',
-  imports: [NavigationComponent, MasterheadComponent, AboutComponent, ServiceComponent, ProjectComponent, HowWorksComponent, ContactUsComponent],
+  imports: [NavigationComponent, MasterheadComponent, AboutComponent, ServiceComponent, ProjectComponent,  ContactUsComponent, WhatappLinkComponent, FooterComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
